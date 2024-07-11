992b694ea53a4d417e0c2f319de077051842e178
zulfikardito <92732353+zulfikardito@users.noreply.github.com>
2024-07-11 14:50:22 +0100

    Add files via upload

---
+++ data/SuttonNightWatchOffice_11.js
@@ -0,0 +1,1 @@
+var json_SuttonNightWatchOffice_11 = {"type":"FeatureCollection","name":"SuttonNightWatchOffice_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"name":"Sutton Night Watch","descriptio":"Outreach\nsocial facility for rough sleepers"},"geometry":{"type":"Point","coordinates":[-0.1527053,51.366188]}}]}
